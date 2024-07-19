deef3af3126491c8f2d8d9f90e614974f7347e1f
Rui <rui.ferreira.v@gmail.com>
2024-07-19 12:20:51 -0300

    Add aulas.

---
+++ Aulas/aula27_fatiamento.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatimanto [i:f:p] [::]
+Obs. : a função	len retorna a qtd
+de caracteres da str
+"""
+variavel = "Olá mundo"
+primeira_letra = (variavel[:1])
+print(primeira_letra)
+print(len(variavel))
+print(variavel[0:9:1])
+print(variavel[::-1])
---
+++ Aulas/aula28_exercicio4.py
@@ -0,0 +1,54 @@
+"""
+Exercícios
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome_invertido}
+        Seu nome contém (ou não ) espaços
+        Seu nome tem {quantidade_letras} letras
+        A primeira letra do seu nome é {primeira_letra}
+        A última letra do seu nome é {ultima_letra}
+Se nada for digitado em nome ou idade:
+    Exiba:
+        "Desculpe, você deixou campos vazios."
+"""
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+if nome and idade:
+    print(f"Seu nome é {nome}")
+    print(f"Seu nome invertido é {nome[::-1]}")
+    if " " in nome:
+        print("Seu nome contém espaço.")
+    else:
+        print("Seu nome não contém espaço.")
+    print(f"Seu nome tem {len(nome)} letras.")
+    print(f"A primeira letra do seu nome é ({(nome[:1])}).")
+    print(f"A última letra do seu nome é ({(nome[-1])}).")
+else:
+    print("Desculpe, você deixou campos vazios.")
+    print("Por favor, preencha os campos para ver os resultados.")
+
+# nome = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
+# nome_invertido = nome[::-1]
+# quantidade_letras = len(nome)
+# primeira_letra = (nome[:1])
+# ultima_letra = (nome[-1])
+
+# if nome and idade:
+#     print(f"Seu nome é {nome}")
+#     print(f"Seu nome invertido é {nome_invertido}")
+#     if " " in nome:
+#         print("Seu nome contém espaço.")
+#     else:
+#         print("Seu nome não contém espaço.")
+#     print(f"Seu nome tem {quantidade_letras} letras.")
+#     print(f"A primeira letra do seu nome é ({primeira_letra}).")
+#     print(f"A última letra do seu nome é ({ultima_letra}).")
+# else:
+#     print("Desculpe, você deixou campos vazios.")
+#     print("Por favor, preencha os campos para ver os resultados.")
