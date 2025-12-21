26f3394c21178caa1a8d3324407273208ee4cb35
Antonio Rui <rui.ferreira.v@gmail.com>
2025-12-21 00:16:39 -0300

    Adiciona novo exercício

---
+++ Aulas/aula29_constante.py
@@ -0,0 +1,12 @@
+"""
+CONSTANTE = "Varaáveis que não vão mudar"
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+    """
+    
+velocidade = 61  # Velocidade atual do carro
+local_carro = 100  # Local em que o carro está na estrada
+
+RADAR_1 = 60  # Velocidade do radar 1
+LOCAL_RADAR_1 = 100  # Local do radar 1 na estrada
+RADAR_RANGE = 1  # A distância que o radar pega
